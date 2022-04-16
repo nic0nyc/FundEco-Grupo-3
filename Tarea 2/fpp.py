bf4cdc12754bbce59a3a9e30b994f13bb12d0d44
Nicolás Villegas Castillo <navillegas@miuandes.cl>
2022-04-16 13:05:57 -0400

    Actualización código para sector económico.

--- Tarea 2/fpp.py
+++ Tarea 2/fpp.py
@@ -1,27 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from ipywidgets import interact, fixed
 
-# initial parameters
-ALPHA = 0.8   # capital share in agriculture
-BETA = 0.4   # capital share in manufacturing
+# Parametros para el análisis
+
+coeficienteTrabajoEconomiaChilena = 0.5
+coeficienteTrabajoConstruccion = 0.8
+
+
+#Parametros del código
+ALPHA = 1 - coeficienteTrabajoEconomiaChilena   # capital share in economia chilena
+BETA = 1 - coeficienteTrabajoConstruccion   # capital share in construccion
 KBAR = 100  # Total capital supply
 LBAR = 100  # Total labor supply
-p = 1      # =Pa/Pm  relative price of ag goods
 
-def F(K,L,alpha=ALPHA):
-    """Agriculture Production function"""
-    return (K**alpha)*(L**(1-alpha))
+"""Función Producción Economía Chilena"""
+def FuncionProduccionEconomiaChilena(capital, labour, alpha):
+    
+    return (capital**alpha)*(labour**(1 - alpha))
 
-def G(K,L,beta=BETA):
-    """Manufacturing Production function"""
-    return (K**beta)*(L**(1-beta))
-
-def budgetc(c1, p1, p2, I):
-    return (I/p2)-(p1/p2)*c1
-
-def isoq(L, Q, mu):
-    return (Q/(L**(1-mu)))**(1/mu)
+"""Función Producción Industria de la Construcción"""
+def FuncionProduccionConstruccion(capital, labour, beta):
+    
+    return (capital**beta)*(labour**(1 - beta))
 
 def edgeworth(L, Kbar=KBAR, Lbar=LBAR,alpha=ALPHA, beta=BETA):
     """efficiency locus: """
@@ -35,21 +35,22 @@ def ppf(LA,Kbar=KBAR, Lbar=LBAR,alpha=ALPHA,beta=BETA):
     arguments:
     LA -- labor allocated to ag, from which calculate QA(Ka(La),La) 
     """
-    KA = edgeworth(LA, Kbar, Lbar,alpha, beta)
+
+    KA = edgeworth(LA, Kbar, Lbar, alpha, beta)
     RTS = (alpha/(1-alpha))*(KA/LA)
-    QA = F( KA,LA,alpha)
-    QM = G(Kbar-KA,Lbar-LA,beta)
-    ax.scatter(QA,QM)
+    QA = FuncionProduccionEconomiaChilena(KA, LA, alpha)
+    QM = FuncionProduccionConstruccion(Kbar-KA, Lbar-LA, beta)
+    #ax.scatter(QA,QM)
     La = np.arange(0,Lbar)
-    Ka = edgeworth(La, Kbar, Lbar,alpha, beta)
-    Qa = F(Ka,La,alpha)
-    Qm = G(Kbar-Ka,Lbar-La,beta)
+    Ka = edgeworth(La, Kbar, Lbar, alpha, beta)
+    Qa = FuncionProduccionEconomiaChilena(Ka, La, alpha)
+    Qm = FuncionProduccionConstruccion(Kbar-Ka, Lbar-La, beta)
     ax.set_xlim(0, Lbar)
     ax.set_ylim(0, Kbar)
-    ax.plot(Qa, Qm,'k-')
-    ax.set_xlabel(r'$Q_A$',fontsize=18)
-    ax.set_ylabel(r'$Q_M$',fontsize=18)
+    ax.plot(Qa, Qm, 'k-')
+    ax.set_xlabel(r'$Q_E$', fontsize=18)
+    ax.set_ylabel(r'$Q_C$', fontsize=18)
     plt.show()
 
 fig, ax = plt.subplots(figsize=(7,6))
-ppf(30,alpha=0.8,beta=0.5)
+ppf(1)
